b65e69a7c69a882367e11cfd28231a29b2f8af56
Sean Martin <sean@seanmartinstudio.com>
2022-08-10 12:57:11 -0700

    Come back to custom

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate: click_bait
+
+   TITLE_ARR = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+
+  def click_bait
+    if TITLE_ARR.none? { |element| element.match(title)}
+      errors.add(:title, "must be clickbait")
+    end
+
 end
